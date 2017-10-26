2445976f7eae3f73b21e3dfd47e80e8b2cdf8a53
Chen Yushen <chenyushen1989@163.com>
2017-10-26 10:53:36 +0800

    time
    time

---
+++ STM32F407/7 time/HARDWARE/TIME/time.c
@@ -0,0 +1,33 @@
+#include "time.h"
+
+
+void TIM3_Int_Init(u16 arr,u16 psc)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 ,ENABLE);//使能TIME3
+	
+	TIM_TimeBaseInitStructure.TIM_ClockDivision=TIM_CKD_DIV1;
+	TIM_TimeBaseInitStructure.TIM_CounterMode=TIM_CounterMode_Up;
+	TIM_TimeBaseInitStructure.TIM_Period=arr;
+	TIM_TimeBaseInitStructure.TIM_Prescaler=psc;
+	//TIM_TimeBaseInitStructure.TIM_RepetitionCounter 高级定时器用	
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);	
+	
+	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);//允许定时器3更新中断
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn ;//中断通道
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;//抢占优先级3
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority =3;		//子优先级3
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
+	NVIC_Init(&NVIC_InitStructure);		
+	
+	TIM_Cmd(TIM3, ENABLE);//使能定时器3
+}
+
+void TIM3_IRQHandler (void)
+{
+	if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update)==SET)
+		LED1=!LED1;
+	TIM_ClearITPendingBit(TIM3, TIM_FLAG_Update);
+}
--- STM32F407/7 time/USER/main.c
+++ STM32F407/7 time/USER/main.c
@@ -3,30 +3,21 @@
 #include "key.h"
 #include "beep.h"
 #include "led.h"
-#include "exti.h"
 #include "usart.h"
-#include "wwdg.h"
+#include "time.h"
 
 
 int main(void)
 {
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	delay_init(168);
-	uart_init(115200);
-	KEY_Init();
 	LED_Init();
-	BEEP_Init();
-	EXTI_init();
-	printf("BOOTING\r\n");
-	LED0=0;
-	delay_ms(300);//延时，重启看到灯亮
-	
-	WWDG_init(0x7F,0x5F,WWDG_Prescaler_8);
+	TIM3_Int_Init(5000-1,8400-1);	//记数5000是500ms
+
 	while(1)
 	{
-		LED0=1;
-		printf("OK\r\n");
-		delay_ms(1000);
+		LED0=!LED0;
+		delay_ms(200);
 	}	
 }
 
