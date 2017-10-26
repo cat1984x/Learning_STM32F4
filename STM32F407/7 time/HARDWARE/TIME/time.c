756870c964e41448fbd20fce038e76638878badc
Chen Yushen <chenyushen1989@163.com>
2017-10-26 10:54:22 +0800

    time
    time

--- STM32F407/7 time/HARDWARE/TIME/time.c
+++ STM32F407/7 time/HARDWARE/TIME/time.c
@@ -16,7 +16,7 @@ void TIM3_Int_Init(u16 arr,u16 psc)
 	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);	
 	
 	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);//允许定时器3更新中断
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn ;//中断通道
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn ;//中断通道//需要跟换为响应中断通道
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;//抢占优先级3
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority =3;		//子优先级3
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
